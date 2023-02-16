e613dd505081d9b90548b12f6feb1d734e717ded
Riley Timlin <122389474+timlinriley@users.noreply.github.com>
2023-02-16 15:29:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let grade = 61
+if (grade >= 80){
+  console.log("You did a good job")
+}
+else if (grade >= 70){
+  console.log("Keep trying")
+  }
+else if (grade >= 60){
+  console.log("ehhhh")
+}
+else if ( grade >= 55){
+  console.log("Not to good")
+}
+else {
+  console.log("You'll get em next time")
+}
+
+for (i =0; i<=100; i++)
+  if (i % 10 === 0){
+    console.log(i)
+  }
